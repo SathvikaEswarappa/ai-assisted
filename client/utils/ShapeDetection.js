e5cf8e391caf2605c92bf89203c17b0354815ba7
Sathvika E <esathvika2005@gmail.com>
2025-10-04 13:12:32 +0530

    Create ShapeDetection.js

---
+++ client/utils/ShapeDetection.js
@@ -0,0 +1,8 @@
+exports.detectShapes = (imageData) => {
+  // Placeholder: return mocked shapes
+  return [
+    { type: 'circle', x: 150, y: 200, radius: 40 },
+    { type: 'rectangle', x: 300, y: 100, width: 120, height: 80 },
+    { type: 'arrow', start: [100, 100], end: [200, 200] }
+  ];
+};
